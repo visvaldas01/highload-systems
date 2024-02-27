eaf98a2279d8e4e192d0023bf98ecae3c1d029e2
YOILLO <yar.slav.ka2002@ya.ru>
2024-02-27 22:10:18 +0300

    feat: add scrobble tests

---
+++ scrobble/src/test/java/ru/ifmo/highloadsystems/tests/ScrobbleTest.java
@@ -0,0 +1,37 @@
+package ru.ifmo.highloadsystems.tests;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Flux;
+import ru.ifmo.highloadsystems.model.dto.ScrobbleDto;
+import ru.ifmo.highloadsystems.model.dto.SongDto;
+import ru.ifmo.highloadsystems.model.entity.Scrobble;
+import ru.ifmo.highloadsystems.service.ScrobbleService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("test")
+public class ScrobbleTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void getAllTest() {
+        webTestClient.get()
+                .uri("/scrobbles")
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
